b8c292b5730269bd8bd602cc44218d6bfb0a891f
Salt Developer <salt-dev@DEV045.local>
2022-03-18 12:09:35 +0100

    removed an empty folder - assets

--- server/utils.js
+++ server/utils.js
@@ -1,23 +1,21 @@
 import fetch from 'node-fetch';
 import dotenv from 'dotenv'
 dotenv.config();
-// console.log(process.env) 
 export const getImages = async (query) => {
     const result = await fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.SECRET_KEY}&query=${query}`)
       .then(res => res.json())
       .then(test => {
         const array = [];
-        // console.log(test);
         test.results.forEach((elem) => {
-          console.log(elem)
-          const imgObject = {descr: elem.alt_description, url: elem.urls.small, download: elem.links.download, username: elem.user.name, user_bio: elem.user.bio, user_image: elem.user.profile_image.large}
+          const imgObject = {descr: elem.alt_description,
+            url: elem.urls.small,
+            download: elem.links.download,
+            username: elem.user.name,
+            user_bio: elem.user.bio,
+            user_image: elem.user.profile_image.large}
           array.push(imgObject);
-          //   console.log(array)
         }); 
         return array
     })
-    console.log(result)
     return result
 }
-
-getImages('dog')
